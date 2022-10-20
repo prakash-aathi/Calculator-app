afbb6d51324cb237b307cf123050a7621518239c
Prakash A <prakashaathi321@gmail.com>
2022-10-20 22:34:56 +0530

    ui fix

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ import './style.css'
 
 let buttons = document.querySelectorAll(".btnstyle");
 let displayEl = document.getElementById("display");
-displayEl.blur();
 
 // listening buttons click and display  
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
-        displayEl.value += e.target.innerText;
+        displayEl.innerText += e.target.innerText;
     });
 });
 
@@ -15,7 +14,7 @@ buttons.forEach(button => {
 let clearEl = document.getElementById("clearAll")
 clearEl.addEventListener("click", clear);
 function clear() {
-    displayEl.value = "";
+    displayEl.innerText = "";
     answerEl.innerText = "";
 }
 
@@ -25,7 +24,7 @@ let answerEl = document.getElementById("answer")
 equal.addEventListener('click', calculate);
 function calculate() {
     try {
-        let result = eval(displayEl.value);
+        let result = eval(displayEl.innerText);
         answerEl.innerText = result;
     }
     catch {
@@ -38,28 +37,27 @@ function calculate() {
 // console.log(buttons[0].click());
 // buttons[0].addEventListener("click", alert("okay"));
 document.addEventListener("keypress", (e) => {
-    console.log("helo");
     switch (e.key) {
         case 'c':
             clear();
             break;
         case '(':
-            displayEl.value+="("
+            displayEl.innerText+="("
             break;
         case ')':
-            displayEl.value+=")"
+            displayEl.innerText+=")"
             break;
         case '/':
-            displayEl.value+="/"
+            displayEl.innerText+="/"
             break;
         case '*':
-            displayEl.value+="*"
+            displayEl.innerText+="*"
             break;
         case '-':
-            displayEl.value+="-"
+            displayEl.innerText+="-"
             break;
         case '+':
-            displayEl.value+="+"
+            displayEl.innerText+="+"
             break;
         case "Enter":
             calculate()
@@ -68,37 +66,53 @@ document.addEventListener("keypress", (e) => {
             calculate()
             break;
         case '.':
-            displayEl.value+="."
+            displayEl.innerText+="."
             break;
         case '0':
-            displayEl.value+="0"
+            displayEl.innerText+="0"
             break;
         case '1':
-            displayEl.value+="1"
+            displayEl.innerText+="1"
             break;
         case '2':
-            displayEl.value+="2"
+            displayEl.innerText+="2"
             break;
         case '3':
-            displayEl.value+="3"
+            displayEl.innerText+="3"
             break;
         case '4':
-            displayEl.value+="4"
+            displayEl.innerText+="4"
             break;
         case '5':
-            displayEl.value+="5"
+            displayEl.innerText+="5"
             break;
         case '6':
-            displayEl.value+="6"
+            displayEl.innerText+="6"
             break;
         case '7':
-            displayEl.value+="7"
+            displayEl.innerText+="7"
             break;
         case '8':
-            displayEl.value+="8"
+            displayEl.innerText+="8"
             break;
         case '9':
-            displayEl.value+="9"
-            break;     	
+            displayEl.innerText+="9"
+            break;  
+        case 'b':
+            slice()
+            break;  
     }
 });
+
+// backspace button
+let backbtn = document.getElementById("back")
+function slice(params) {
+    let temp = displayEl.innerText.slice(0, -1);
+    displayEl.innerHTML = temp;
+    
+
+    if (temp.length == 0) {
+        answerEl.innerText = "";
+    }
+}
+backbtn.addEventListener("click", slice);
